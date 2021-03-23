d0d4ca31d48ccc21982dff1a6a49a2bbef106d35
Adam Megger <adammegger@int.pl>
2021-03-23 18:31:57 +0100

    Add files via upload
    deploy update

---
+++ build/precache-manifest.8ba29dd17ad963e40674072df0f39df1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4296cf3a21a465509ab374d2edf4cee6",
+    "url": "/Star-Wars-mini-Encyclopedia/index.html"
+  },
+  {
+    "revision": "0970ac9f7054d1cb145a",
+    "url": "/Star-Wars-mini-Encyclopedia/static/css/main.14b948fe.chunk.css"
+  },
+  {
+    "revision": "1eb3faf83f5dc7d632a4",
+    "url": "/Star-Wars-mini-Encyclopedia/static/js/2.fe8e0101.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Star-Wars-mini-Encyclopedia/static/js/2.fe8e0101.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0970ac9f7054d1cb145a",
+    "url": "/Star-Wars-mini-Encyclopedia/static/js/main.bdfc41d5.chunk.js"
+  },
+  {
+    "revision": "0525d55b9514301ffb1a74f1b5946a69",
+    "url": "/Star-Wars-mini-Encyclopedia/static/js/main.bdfc41d5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cdb1d0a598ae50f5ec94",
+    "url": "/Star-Wars-mini-Encyclopedia/static/js/runtime-main.c037bf81.js"
+  },
+  {
+    "revision": "eeba7aabc3cac4f673a7aa66fbeb9cb8",
+    "url": "/Star-Wars-mini-Encyclopedia/static/media/logo.eeba7aab.png"
+  }
+]);
